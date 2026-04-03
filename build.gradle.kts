91eed38e95fa60f2288dabb767deb641e457c40d
Lallu Anthoor <developer@lanthoor.dev>
2026-04-03 19:45:47 +0530

    fix: downgrade kotlin and use sonar step

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,16 +5,4 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.android) apply false
-    id("org.sonarqube") version "7.2.3.7755"
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "lanthoor_spendly")
-        property("sonar.organization", "lanthoor")
-        property(
-            "sonar.coverage.jacoco.xmlReportPaths",
-            "${rootProject.projectDir}/app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
-        )
-    }
 }
